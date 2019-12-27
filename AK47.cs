bcfe9968b93bda6596ee1f7a63d3869444d2c2ad
Bertil Andersen <bertilandersen@hotmail.com>
2019-12-27 22:43:55 +0100

    Slight  adjustments to interfaces

--- AK47.cs
+++ AK47.cs
@@ -4,33 +4,8 @@ using System.Text;
 
 namespace OpgaveCv3
 {
-    public class AK47 : AbstractWeapon, IShootingWeapon
+    public class AK47 : AbstractBigGun
     {
-        private int amountOfBullits;
-        private int amountOfBullitsInClip;
-        private int clipSize;
-
-        public int AmountOfBullits { get { return amountOfBullits; } }
-
-        public int AmountOfBullitsInClip { get { return amountOfBullitsInClip; } }
-
-        public int ClipSize { get { return clipSize; } }
-
-        public void Reload()
-        {
-            int missingbullitsInClip = clipSize - amountOfBullitsInClip;
-            if (amountOfBullits >= missingbullitsInClip)
-            {
-                amountOfBullitsInClip += missingbullitsInClip;
-                amountOfBullits -= missingbullitsInClip;
-            }
-            else
-            {
-                amountOfBullitsInClip += amountOfBullits;
-                amountOfBullits = 0;
-            }
-        }
-
         public override int DoDamage()
         {
             Console.WriteLine($"pop !!!! Your {this.WeaponName} does {this.Damage} damage to your target!!!!");
---
+++ AbstractBigGun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpgaveCv3
+{
+    public abstract class AbstractBigGun : AbstractWeapon
+    {
+        protected int amountOfBullits;
+        protected int amountOfBullitsInClip;
+        protected int clipSize;
+
+        public int AmountOfBullits { get { return amountOfBullits; } }
+        public int AmountOfBullitsInClip { get { return amountOfBullitsInClip; } }
+        public int ClipSize { get { return clipSize; } }
+
+        public virtual void Reload() {
+            int missingbullitsInClip = clipSize - amountOfBullitsInClip;
+            if (amountOfBullits >= missingbullitsInClip)
+            {
+                amountOfBullitsInClip += missingbullitsInClip;
+                amountOfBullits -= missingbullitsInClip;
+            }
+            else
+            {
+                amountOfBullitsInClip += amountOfBullits;
+                amountOfBullits = 0;
+            }
+        }
+        public AbstractBigGun(string name, int damage) : base(name, damage)
+        {
+        }
+    }
+}
