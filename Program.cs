5aeedaf4bcb2a8de8fcb56b944261e266594d0c3
Bertil Andersen <bertilandersen@hotmail.com>
2020-01-04 13:39:10 +0100

    added a minimal user interface

--- Program.cs
+++ Program.cs
@@ -11,9 +11,38 @@ namespace OpgaveCv3
             UnitBuilder builder = new MarineBuilder();
             UnitDirector director = new MarineUnitDirector();
             IUnit theUnit = director.Build(builder);
+            
+            bool rungame = true;
 
+            Console.WriteLine("A unit has been created - press q to quit, s to switch weapon, a to attack, r to reload");
 
+            while (rungame == true)
+            {
 
+                string input = Console.ReadLine();
+                if (input.ToLower() == "q")
+                {
+                    rungame = false;
+                }
+                else if (input.ToLower() == "s")
+                {
+                    theUnit.switchWeapon();
+                }
+                else if (input.ToLower() == "a")
+                {
+                    theUnit.Attack();
+                }
+                else if (input.ToLower() == "r")
+                {
+                    theUnit.ReloadWeapon();
+                }
+                else
+                {
+                    Console.WriteLine("Woops - something went wrong - try again");
+                }
+            }
+
+            Console.WriteLine("Game ended");
             Console.ReadKey();
         }
     }
